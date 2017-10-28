784b936a995ee5dc556be4a2e6382350c4a45023
askbk <askbk@protonmail.com>
2017-10-28 16:35:12 +0200

    added form handling

---
+++ contact.php
@@ -0,0 +1,34 @@
+<?php
+// saniterer input data
+$name = $email = $phone = $text = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $phone = test_input($_POST["website"]);
+  $text = test_input($_POST["comment"]);
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if(preg_match("#[-a-zA-Z0-9éèÉÈäöæøåÄÖÆØÅ._ ]+#",$name) AND
+   preg_match("#[-a-zA-Z0-9.@+!=()_:]+#",$email) AND
+   strlen($text) < 500) {
+    if(mail("datanet@live.com", // adresse som skal motta eposten
+            "Kontaktskjema", // emne
+            "Melding fra $name <$email>, <$phone>:\n\n$text", // meldingsteksten
+            "From: Tilbakemeldingsskjema <mail@imnor.no>")) // avsender
+    {
+        echo "Meldingen ble sendt!";
+    } else {
+        echo "En feil oppstod!";
+    }
+} else {
+    echo "Ugyldige eller for mange tegn!";
+}
+?>
