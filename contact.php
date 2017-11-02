f4df2fbc01840f2b632a0798f44426fd9dfbb226
askbk <askbk@protonmail.com>
2017-11-02 12:58:44 +0100

    more fixes

--- contact.php
+++ contact.php
@@ -1,13 +1,4 @@
 <?php
-// saniterer input data
-$name = $email = $phone = $text = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $phone = test_input($_POST["phone"]);
-  $text = test_input($_POST["text"]);
-}
 
 function test_input($data) {
     $data = trim($data);
@@ -16,14 +7,27 @@ function test_input($data) {
     return $data;
 }
 
+// saniterer input data
+$name = $email = $phone = $text = $headers = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $phone = test_input($_POST["phone"]);
+  $text = test_input($_POST["text"]);
+}
+
+$to = "inmarket@imnor.no";
+$subject = "Henvendelse fra $name";
+$message = "Melding fra $name <$email>, <$phone>:\n\n$text";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text; charset=UTF-8" . "\r\n";
+$headers .= 'From: Kontaktskjema <' . $email . '>' . "\r\n";
+
 if(preg_match("#[-a-zA-Z0-9éèÉÈäöæøåÄÖÆØÅ._ ]+#",$name) AND
-   preg_match("#[-a-zA-Z0-9.@+!=()_:]+#",$email) AND
-   strlen($text) < 500) {
-    if(mail("inmarket@imnor.no", // adresse som skal motta eposten
-            "Henvendelse gjennom kontaktskjema fra $name", // emne
-            "Melding fra $name <$email>, <$phone>:\n\n$text", // meldingsteksten
-            "From: Tilbakemeldingsskjema <$email>")) // avsender
-    {
+   preg_match("#[-a-zA-Z0-9.@+!=()_:]+#", $email) AND
+   strlen($text) < 1000) {
+    if(mail($to, $subject, $message, $headers)){
         echo "Meldingen ble sendt!";
     } else {
         echo "En feil oppstod!";
