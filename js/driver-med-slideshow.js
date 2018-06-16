a18dfb5cefb4b46f852d365116a7e94ea2e654da
ask <askbk@protonmail.com>
2018-06-16 15:08:41 +0200

    further slideshow adjustments

--- js/driver-med-slideshow.js
+++ js/driver-med-slideshow.js
@@ -1,19 +1,36 @@
 let container = document.getElementById("driver-med-slideshow");
 let slides = document.getElementsByClassName("driver-med-slides");
-let i = 2;
+let bubble1 = document.getElementById("slideshow-bubble-1");
+let bubble2 = document.getElementById("slideshow-bubble-2");
+let bubble3 = document.getElementById("slideshow-bubble-3");
+let i = 2, prev = 0;
 
 var slideInterval = setInterval(slideshow,2500);
 
-slideshow();
+function resetSlideshow() {
+    for(let j = 0; j < 3; ++j) {
+        slides[j].style.display = "none";
+    }
 
+    slides[i].style.display = "block";
+}
 
 function slideshow() {
-
-    let prev = (i++) % 3;
+    prev = (i++) % 3;
     i %= 3;
 
     slides[prev].style.display = "none";
     slides[i].style.display = "block";
+}
+
+function bubbleMouseOver(n) {
+    clearInterval(slideInterval);
+    slides[i].style.display = "none";
+    slides[n - 1].style.display = "block";
+}
+
+function bubbleMouseOut() {
+    resetSlideshow();
 }
 
 container.onmouseover = function() {
@@ -21,5 +38,5 @@ container.onmouseover = function() {
 }
 
 container.onmouseout = function() {
-    slideInterval = setInterval(slideshow,2000);
+    slideInterval = setInterval(slideshow,2500);
 }
