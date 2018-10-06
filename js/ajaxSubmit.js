e285097b711da7ed86f1ad7f1e0c0fa69f11cb19
ask <askbk@protonmail.com>
2018-10-06 16:51:34 +0200

    Fikset kontaktskjema litt + fikset feil i menyen.

--- js/ajaxSubmit.js
+++ js/ajaxSubmit.js
@@ -1,33 +1,7 @@
 // Submit form using AJAX
-let contactForm = document.getElementById("kontaktSkjema");
 
-contactForm.onsubmit = function (event) {
-	event.preventDefault();
-}
-
-function submitForm() {
-	let client = encodeURIComponent(document.querySelector('input[name="klient"]:checked').value);
-	let name = encodeURIComponent(document.getElementById("inputName").value);
-	let place = encodeURIComponent(document.getElementById("inputPlace").value);
-	let phone = encodeURIComponent(document.getElementById("inputPhone").value);
-	let data = "client=" + client + "&name=" + name + "&place=" + place + "&phone=" + phone;
-	let xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			if (this.responseText != "Takk for henvendelsen!") {
-				document.getElementById("responseText").innerHTML = this.responseText;
-			} else {
-				document.getElementById("kontaktSkjema").classList.add("w3-hide");
-				document.getElementById("kontaktRespons").classList.remove("w3-hide");
-			}
-		}
-	}
-	xhttp.open("POST", "contact.php", true);
-	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-	xhttp.send(data);
-}
-
-function submitPhone() {
+$("#phoneSubmit").click(function (e) {
+	e.preventDefault();
 	let phone = encodeURIComponent(document.getElementById("inputPhoneBar").value);
 
 	if(phone.indexOf(' ') >= 0 || phone == '') {
@@ -42,15 +16,15 @@ function submitPhone() {
 				//document.getElementById("responseText").innerHTML = this.responseText;
 			} else {
 				console.log("success");
-				document.getElementById("navbar-contact").className += " w3-hide ";
-				document.getElementById("navbar-submit").className += " w3-show";
+				document.getElementById("phone-input").classList.add("w3-hide");
+				document.getElementById("navbar-submit").classList.remove("w3-hide");
 			}
 			console.log("success");
-			document.getElementById("navbar-contact").className += " w3-hide ";
-			document.getElementById("navbar-submit").className += " w3-show";
+			document.getElementById("phone-input").classList.add("w3-hide");
+			document.getElementById("navbar-submit").classList.remove("w3-hide");
 		}
 	}
 	xhttp.open("POST", "contactPhone.php", true);
 	xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
 	xhttp.send(data);
-}
+});
