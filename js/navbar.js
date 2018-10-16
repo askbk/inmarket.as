04aeeeb9a40aa33896d7604dc0a6b20cfb823e02
ask <askbk@protonmail.com>
2018-10-16 12:46:40 +0200

    Ga menyen enda bedre stil ;)

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,8 @@
 $("#navbar-button").click(function () {
-    $(".menu-container").toggleClass("menu-change");
-    $("#navbar-dropdown").slideToggle("fast");
     if ($('p,h1,h2,h3,h4,h5').css('visibility') == 'hidden') { // Dropdown was already down
-        // $("#navbar-dropdown").hide("slow");
-        $('p,h1,h2,h3,h4,h5').css('visibility','visible');
-        if ($(window).scrollTop() > 100) {
-            $(".hide-scroll").fadeOut();
-        }
-        $("#menu-opened").hide();
-        $("#menu-closed").show();
+        hideMenu();
     } else {
-        // $("#navbar-dropdown").show("slow");
-        $('p,h1,h2,h3,h4,h5').css('visibility','hidden');
-        $("#menu-closed").hide();
-        $("#menu-opened").show();
-        $(".hide-scroll").fadeIn();
+        showMenu();
     }
 });
 
@@ -27,3 +15,31 @@ $(window).scroll(function () {
         $(".hide-scroll").fadeIn();
     }
 });
+
+$(".w3-overlay").click(function () {
+    hideMenu();
+});
+
+function hideMenu() {
+    $(".menu-container").toggleClass("menu-change");
+    // $("#temp1").slideToggle("fast");
+    $("#navbar-dropdown-content").slideToggle("fast");
+    $(".w3-overlay").hide();
+    $('p,h1,h2,h3,h4,h5').css('visibility','visible');
+    if ($(window).scrollTop() > 100) {
+        $(".hide-scroll").fadeOut();
+    }
+    $("#menu-opened").hide();
+    $("#menu-closed").show();
+}
+
+function showMenu() {
+    $(".menu-container").toggleClass("menu-change");
+    // $("#temp1").slideToggle("fast");
+    $("#navbar-dropdown-content").slideToggle("fast");
+    $(".w3-overlay").fadeIn();
+    $('p,h1,h2,h3,h4,h5').css('visibility','hidden');
+    $("#menu-closed").hide();
+    $("#menu-opened").show();
+    $(".hide-scroll").fadeIn();
+}
