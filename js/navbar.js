d48cc0ffc3a41bb71f8fb9ce54a8dd7a26cab62c
askbk <askbk@protonmail.com>
2018-10-17 15:47:04 +0200

    Forbedringer i JS

--- js/navbar.js
+++ js/navbar.js
@@ -1,45 +1,30 @@
+let i = 0,
+    menuContainer = $(".menu-container"),
+    overlay = $(".w3-overlay"),
+    dropdown = $("#navbar-dropdown-content");
+
 $("#navbar-button").click(function () {
-    if ($('p,h1,h2,h3,h4,h5').css('visibility') == 'hidden') { // Dropdown was already down
-        hideMenu();
-    } else {
+    if (i%2===0) {
         showMenu();
-    }
-});
-
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-        if ($("p").css('visibility') != "hidden") {
-            $(".hide-scroll").fadeOut();
-        }
     } else {
-        $(".hide-scroll").fadeIn();
+        hideMenu();
     }
 });
 
-$(".w3-overlay").click(function () {
+overlay.click(function () {
     hideMenu();
 });
 
 function hideMenu() {
-    $(".menu-container").toggleClass("menu-change");
-    // $("#temp1").slideToggle("fast");
-    $("#navbar-dropdown-content").slideToggle("fast");
-    $(".w3-overlay").hide();
-    $('p,h1,h2,h3,h4,h5').css('visibility','visible');
-    if ($(window).scrollTop() > 100) {
-        $(".hide-scroll").fadeOut();
-    }
-    $("#menu-opened").hide();
-    $("#menu-closed").show();
+    ++i;
+    menuContainer.toggleClass("menu-change");
+    dropdown.slideToggle("fast");
+    overlay.hide();
 }
 
 function showMenu() {
-    $(".menu-container").toggleClass("menu-change");
-    // $("#temp1").slideToggle("fast");
-    $("#navbar-dropdown-content").slideToggle("fast");
-    $(".w3-overlay").fadeIn();
-    $('p,h1,h2,h3,h4,h5').css('visibility','hidden');
-    $("#menu-closed").hide();
-    $("#menu-opened").show();
-    $(".hide-scroll").fadeIn();
+    ++i;
+    menuContainer.toggleClass("menu-change");
+    dropdown.slideToggle("fast");
+    overlay.fadeIn();
 }
