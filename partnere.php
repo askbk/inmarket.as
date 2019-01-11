475a8d678c67e6abdbbf46cea7791219e7119861
askbk <askbk@protonmail.com>
2019-01-11 08:50:00 +0100

    La head-seksjonen inn i egen php-fil

--- partnere.php
+++ partnere.php
@@ -2,41 +2,8 @@
 <html>
 
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <title>Samarbeidspartnere | InMarket Norge</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-    <meta name="description" content="Rådgivning kombinert med jobbskygging og mentoring. Møt fremtiden din og la den forme deg.">
-
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="mask-icon" href="safari-pinned-tab.svg" color="#c08d42">
-    <link rel="shortcut icon" href="favicon.ico">
-    <meta name="msapplication-TileColor" content="#2b5797">
-    <meta name="theme-color" content="#ffffff">
-
-    <link rel="stylesheet" href="fontawesome-free-5.1.0-web/css/all.css">
-    <link rel="stylesheet" href="style/style.css" />
-    <link rel="stylesheet" href="style/w3pro.css" />
-    <script type="application/ld+json" href="json/about.json"></script>
-    <script src="js/jquery-3.3.1.min.js" charset="utf-8"></script>
-    <script src="js/jquery.color-2.1.2.min.js" charset="utf-8"></script>
-    <!-- Hotjar Tracking Code for imnor.no -->
-    <script>
-        if (localStorage.tracking == "true") {
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:959986,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-                console.log("hotjar enabled");
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        }
-    </script>
+    <?php include_once 'templates/head.php'; ?>
 </head>
 
 <body>
