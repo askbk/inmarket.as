4973275a69747e561952c7c569d897f555368c82
askbk <askbk@protonmail.com>
2019-01-12 00:01:57 +0100

    Begynte på side for elever og studenter

---
+++ elever-studenter.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Studenter og elever | InMarket Norge</title>
+    <?php include_once 'templates/head.php'; ?>
+</head>
+
+<body>
+    <?php
+    include_once 'templates/navigation.php';
+    include_once 'templates/cookie_notice.php';
+    ?>
+    <div class="margin-top-100 full-height w3-row">
+        <div class="w3-col s6 height-100 w3-display-container">
+            <div class="bgimg-glade-pt1">
+                Trykk her: elev
+            </div>
+            <div class="w3-card height-100 w3-large w3-padding white-bg w3-display-right max-width-500" style="display:none;">
+                <p>InMarket ønsker å samarbeide med din utdanningsplass for at du skal få den beste muligheten til å forberede deg til arbeidslivet.​</p>
+                <p>Med oss kan du utvikle de egenskapene som får deg til å blomstre. Vi hjelper deg med å bygge erfaringer og kompetanse som er relevant for studier og karriere.​</p>
+                <p>Vi tilbyr frivillige stillinger som praktikanter i bedrifter, eller lønnede trainee stillinger. Arbeidstiden vil varier mellom 5 arbeidstimer i uken til 22 arbeidstimer i uken.​</p>
+                <a href="#" class="black-button w3-card">SE OG SØK PÅ ÅPNE PROSJEKTPLASSER I DITT OMRÅDE</a>
+            </div>
+        </div>
+        <div class="w3-col s6 height-100 w3-display-container">
+            <div class="w3-card height-100 w3-large w3-padding white-bg w3-display-left max-width-500" style="display:none;">
+                <p>Vi holder oss tett koblet til utdanningsplassen din slik at du ikke behøver å bortprioritere dine studier. Ved å gjøre noe som aktivt kan kobles til studie ditt, vil du stå sterk til å velge veien din videre.​</p>
+                <p>Som elev kan du kobles til høyere studier som kan være av interesse å få informasjon om tjenester og verv, erfaring i relevant virksomhet med mer.</p>
+                <p>Vi kommuniserer med skolen din slik at du kan få faste tidspunkter i uken eller i måneden sammen med oss og våre samarbeidspartnere.​</p>
+                <a href="#" class="black-button w3-card">SE OG SØK PÅ ÅPNE PROSJEKTPLASSER I DITT OMRÅDE</a>
+            </div>
+            <div class="bgimg-glade-pt2">
+                Trykk her: student
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <?php include_once 'templates/footer.php'; ?>
+    <script src="js/detectIphone.js" charset="utf-8"></script>
+    <script async src="js/navbar.js" charset="utf-8"></script>
+    <script src="js/acceptTracking.js" charset="utf-8"></script>
+</body>
+
+</html>
