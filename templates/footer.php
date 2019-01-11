7b12c15cfed3b7b2296d36433c02446c503e4c10
askbk <askbk@protonmail.com>
2019-01-11 09:02:00 +0100

    Endret bunnlinjetekst

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="w3-container">
+<footer class="w3-container uppercase">
     <div class="w3-content w3-row golden-text padding-left-100 w3-row-padding">
         <div class="w3-col s4">
             <h3 class="">Oss</h3>
@@ -8,10 +8,10 @@
                     <a href="teamet.php">Vårt team</a>
                 </li>
                 <li class="remove-borders remove-padding">
-                    <a href="teamet.php#blimed">Bli med</a>
+                    <a href="teamet.php#blimed">Hva skjer hos oss?</a>
                 </li>
                 <li class="remove-borders remove-padding">
-                    <a href="aktiviteter.php">Aktiviteter</a>
+                    <a href="aktiviteter.php">Visjon og mål</a>
                 </li>
             </ul>
         </div>
@@ -43,12 +43,12 @@
         <div class="w3-col s4">
             <h3 class="golden-text">Hjelp</h3>
             <ul class="w3-ul w3-large">
-                <!-- <li class="remove-borders remove-padding">
-                    <a href="#">Kundeservice</a>
-                </li>
                 <li class="remove-borders remove-padding">
                     <a href="#">Vanlige spørsmål og svar</a>
-                </li> -->
+                </li>
+                <li class="remove-borders remove-padding">
+                    <a href="#">Kundestøtte</a>
+                </li>
                 <li class="remove-borders remove-padding">
                     <a href="https://www.office.com/?auth=2&home=1">Office 365</a>
                 </li>
