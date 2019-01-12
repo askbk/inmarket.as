35f1e7f09e3133aa6738c227942270ff85d3bf11
askbk <askbk@protonmail.com>
2019-01-12 11:23:57 +0100

    Lagde animasjon for todeling av bilde

--- elever-studenter.php
+++ elever-studenter.php
@@ -11,12 +11,12 @@
     include_once 'templates/navigation.php';
     include_once 'templates/cookie_notice.php';
     ?>
-    <div class="margin-top-100 full-height w3-row">
+    <div class="margin-top-100 full-height w3-row" style="height: calc(100vh - 100px);">
         <div class="w3-col s6 height-100 w3-display-container">
-            <div class="bgimg-glade-pt1">
+            <div class="bgimg-glade-pt1 z-index-1">
                 Trykk her: elev
             </div>
-            <div class="w3-card height-100 w3-large w3-padding white-bg w3-display-right max-width-500" style="display:none;">
+            <div class="w3-card height-100 w3-large w3-padding white-bg width-500 sliding-text-right rightn-500 z-index-1" id="student-text">
                 <p>InMarket ønsker å samarbeide med din utdanningsplass for at du skal få den beste muligheten til å forberede deg til arbeidslivet.​</p>
                 <p>Med oss kan du utvikle de egenskapene som får deg til å blomstre. Vi hjelper deg med å bygge erfaringer og kompetanse som er relevant for studier og karriere.​</p>
                 <p>Vi tilbyr frivillige stillinger som praktikanter i bedrifter, eller lønnede trainee stillinger. Arbeidstiden vil varier mellom 5 arbeidstimer i uken til 22 arbeidstimer i uken.​</p>
@@ -24,17 +24,30 @@
             </div>
         </div>
         <div class="w3-col s6 height-100 w3-display-container">
-            <div class="w3-card height-100 w3-large w3-padding white-bg w3-display-left max-width-500" style="display:none;">
+            <div class="w3-card height-100 w3-large w3-padding white-bg w3-display-left width-500 sliding-text-left z-index-1" id="pupil-text">
                 <p>Vi holder oss tett koblet til utdanningsplassen din slik at du ikke behøver å bortprioritere dine studier. Ved å gjøre noe som aktivt kan kobles til studie ditt, vil du stå sterk til å velge veien din videre.​</p>
                 <p>Som elev kan du kobles til høyere studier som kan være av interesse å få informasjon om tjenester og verv, erfaring i relevant virksomhet med mer.</p>
                 <p>Vi kommuniserer med skolen din slik at du kan få faste tidspunkter i uken eller i måneden sammen med oss og våre samarbeidspartnere.​</p>
                 <a href="#" class="black-button w3-card">SE OG SØK PÅ ÅPNE PROSJEKTPLASSER I DITT OMRÅDE</a>
             </div>
-            <div class="bgimg-glade-pt2">
+            <div class="bgimg-glade-pt2 z-index-1">
                 Trykk her: student
             </div>
         </div>
     </div>
+
+    <script type="text/javascript">
+        $(".bgimg-glade-pt2").on("click", function () {
+            $("#student-text").addClass("right-0")
+            $(".bgimg-glade-pt1").addClass("translateX-n500");
+        });
+        $(".bgimg-glade-pt1").on("click", function () {
+            $(".right-0").removeClass("right-0");
+            $(".bgimg-glade-pt1").removeClass("translateX-n500");
+        });
+
+    </script>
+
     <!-- Footer -->
     <?php include_once 'templates/footer.php'; ?>
     <script src="js/detectIphone.js" charset="utf-8"></script>
