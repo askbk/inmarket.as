ed774d138a215b3df59e2af5c45d18329aaab2e6
askbk <askbk@protonmail.com>
2019-01-20 14:22:03 +0100

    Lagde workshop-fil ;)

---
+++ workshop.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Form din workshop | InMarket Norge</title>
+    <?php include_once 'templates/head.php'; ?>
+</head>
+
+<body>
+    <?php
+    include_once 'templates/navigation.php';
+    include_once 'templates/cookie_notice.php';
+    ?>
+    <div class="margin-top-100">
+        <h2 class="golden-bg w3-center">FORM WORKSHOPEN DU ØNSKER DEG​</h2>
+    </div>
+
+    <form class="" action="index.html" method="post">
+        <label for=""></label>
+    </form>
+    <!-- Footer -->
+    <?php include_once 'templates/footer.php'; ?>
+    <script src="js/detectIphone.js" charset="utf-8"></script>
+    <script async src="js/navbar.js" charset="utf-8"></script>
+    <script src="js/acceptTracking.js" charset="utf-8"></script>
+</body>
+
+</html>
