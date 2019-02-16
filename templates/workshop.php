0c18627673e1ca2c129a52d6b0ce1bdac088f4f8
ask <askbk@protonmail.com>
2019-02-16 18:45:50 +0100

    Flyttet workshop-ting til en egen fil

---
+++ templates/workshop.php
@@ -0,0 +1,20 @@
+<div class="w3-display-container parallax-img bgimg-allinclusive-2" style="height:70vh;">
+    <div class="w3-display-bottomright text-shadow w3-jumbo white-text" style="right:2.5em;bottom:2em;">
+        WORKSHOP
+    </div>
+</div>
+
+<div class="black-bg">
+    <article class="w3-content w3-xlarge w3-container white-text">
+        <p>Alle lærer seg teknikker for å skape seg en trygg grunnmur som kan adapteres på alle scener og i alle situasjoner.​</p>
+        <p>Gjennom praktiske øvelser gir vi din bedrift konkrete verktøy og løsninger for å mestre det å kommunisere.</p>
+    </article>
+</div>
+<div class="w3-row">
+    <a href="workshop.php" class="mobile-large w3-col m6 w3-card w3-center golden-button">
+        FORM WORKSHOPEN DU ØNSKER DEG
+    </a>
+    <a href="https://www.allinclusivetalk.com/" target="_blank" class="mobile-large w3-col m6 w3-card w3-center golden-button">
+        OM ALL INCLUSIVE TALK
+    </a>
+</div>
