300df5b71d6c60ada03b565b21cce9415de9c43e
Filip Tangen <filiptangen1998@live.com>
2019-02-19 11:54:39 +0000

    Cards på figurer

--- elever-studenter.php
+++ elever-studenter.php
@@ -58,8 +58,8 @@
             </div>
         </div>
     </div>
-
-    <div class="w3-section" style="max-width:870px;margin:auto;">
+    <br><br>
+    <div class="w3-card" style="max-width:870px;margin:auto; margin-bottom:40px;">
         <div class="w3-padding-32 w3-container w3-center w3-xlarge" style="padding-left:25px;">
             <div class="w3-left w3-mobile" style="width:225px; padding-top:30px;">
                 <img src="img/fig/arbeidssokere.png" alt="Figure" style="width:75%;">
