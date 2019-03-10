d3829adc5f90cf98631f7f1c5da6e0f2bcca1d68
askbk <askbk@protonmail.com>
2019-03-10 12:13:21 +0100

    Oversatte til engelsk/fikset

--- en/teamet.php
+++ en/teamet.php
@@ -14,14 +14,9 @@
 
     <div class="w3-display-container parallax-img bgimg-sammy-oyvind-ask full-height margin-top-100">
         <article class="w3-display-bottommiddle white-text mobile-medium width-100 w3-container max-width-1200 w3-center" style="padding-bottom:50px;">
-            <p>We work together as a team. We want to achieve great things for the Norwegian youth, Norwegian industry
-                and contribute in international growth. We want to be part of the solution Norway uses to increase in
-                diversity greater international impact.</p>
-            <p>InMarket team consists of young leaders and employees working together to create Norway’s largest
-                meeting place for employers and the rest of society.</p>
-            <p>We want the process of acquiring employees not only be about the little time it takes to publish
-                position and finding employees. We want the hiring process to start earlier.</p>
-
+            <p>We work together as a team. We want to achieve great things for the Norwegian youth, Norwegian industry and contribute in international growth. We want to be part of the solution Norway uses to achieve greater international impact.</p>
+            <p>The InMarket team consists of young leaders and employees working together to create Norway’s largest meeting place for employers and the rest of society.</p>
+            <p>We want the process of recruiting employees not only to be about the little time it takes to publish position and finding employees. We want the hiring process to start earlier.</p>
         </article>
     </div>
 
@@ -29,13 +24,13 @@
         <div class="w3-col m6 w3-padding-large w3-container">
             <img src="img/fig/ledelse.jpg" alt="" class="width-25-percent">
             <p>
-                As a Manager in InMarket AS you´re responsible for your department. You´ll have proper working conditions, great opportunities to develop your skill set and you´ll be in direct communication with the board. You´ll be able to guide, organize and follow up your own team and you are helping shape InMarket´s goals and work process.
+                As a manager at InMarket, you're responsible for your department. You'll have proper working conditions, great opportunities to develop your skill set and you'll be in direct communication with the board. You'll be able to guide, organize and follow up your own team and you are helping shape InMarket's goals and work process.
             </p>
         </div>
         <div class="w3-col m6 w3-padding-large w3-container">
             <img src="img/fig/ledelse.jpg" alt="" class="width-25-percent">
             <p>
-                As an employee of InMarket will be able to use your expertise to solve specific tasks in our operation. We are looking primarily for you who understands and has insight into the Norwegian labor market. Everything you do here can be linked to our vision to strengthen the Norwegian labor market in an international context.
+                As an employee at InMarket, you will be able to use your expertise to solve specific tasks in our operation. We are looking primarily those who understand and have insight into the Norwegian labor market. Everything you do here can be linked to our vision to strengthen the Norwegian labor market in an international context.
             </p>
         </div>
     </div>
@@ -74,7 +69,7 @@
                 </div>
                 <div class="hidden-description w3-right">
                     <div class="w3-padding">
-                        <p class="white-space-normal w3-large">Øyvind has one year of experience as a soldier in the Air Force. He’s studying for a master's degree in Electronic system design and innovation at NTNU and has been the general manager of Compendium Enterprise Lumenia. Øyvind wants to challenge the status quo and always looks for opportunities to change attitudes and values in those he encounters. </p>
+                        <p class="white-space-normal w3-large">Øyvind has one year of experience as a soldier in the Air Force. He’s studying for a master's degree in electrical engineering at NTNU and has been the general manager of the compendium company Lumenia. Øyvind wants to challenge the status quo and always looks for opportunities to change attitudes and values in those he encounters.</p>
                         <p><a href="https://www.linkedin.com/in/%C3%B8yvind-fjeld-047444144/" class="text-underline hover-golden-text">Øyvind's profile on LinkedIn </a></p>
                     </div>
                 </div>
@@ -102,13 +97,13 @@
                     <img src="img/portraits/ask.jpg" alt="Teknologidirektør Ask Berstad Kolltveit" width="100%" itemprop="image">
                     <div class="w3-container">
                         <h4 itemprop="name" class="">Ask Berstad Kolltveit</h4>
-                        <p class="w3-opacity" itemprop="jobTitle">IT Director</p>
+                        <p class="w3-opacity" itemprop="jobTitle">CTO</p>
                         <p>Tel: (+47) <span itemprop="telephone">950 01 753</span><br>Email: <span itemprop="email">ask@inmarket.as</span></p>
                     </div>
                 </div>
                 <div class="hidden-description w3-right">
                     <div class="w3-padding">
-                        <p class="white-space-normal w3-large">Ask is a former finalist in the Norwegian Informatics Olympiade. He´s studying for a master's degree in computer technology at NTNU and is primarily responsible for the development of the InMarket websites and the app. Ask has been through the school system in Norway and he wants to give same opportunities he had to other youth. </p>
+                        <p class="white-space-normal w3-large">Ask is a former finalist in the Norwegian Informatics Olympiad. He's studying for a master's degree in computer science at NTNU and is primarily responsible for the development of the InMarket websites and the app. Ask has been through the school system in Norway and he wants to give same opportunities he had to other youth.</p>
                         <p><a href="https://www.linkedin.com/in/ask-berstad-kolltveit-9b568b16a/" class="text-underline hover-golden-text">Ask his profile on LinkedIn </a></p>
                     </div>
                 </div>
@@ -124,7 +119,7 @@
                 </div>
                 <div class="hidden-description w3-right">
                     <div class="w3-padding">
-                        <p class="white-space-normal w3-large">Filip studying for a bachelor’s in computer science at NTNU in Trondheim and is also a student assistant in the subject Object-Oriented Programming. He has been involved in game development from a young age and is now motivated to make a difference in the education system in Norway.</p>
+                        <p class="white-space-normal w3-large">Filip is studying for a bachelor’s in computer science at NTNU in Trondheim and is also a student assistant in the course Object-Oriented Programming. He has been involved in game development from a young age and is now motivated to make a difference in the education system in Norway.</p>
                         <p><a href="https://www.linkedin.com/in/filip-tangen-61614015b/" class="text-underline hover-golden-text">Filip's profile on LinkedIn </a></p>
                     </div>
                 </div>
@@ -140,7 +135,7 @@
                 </div>
                 <div class="hidden-description w3-right">
                     <div class="w3-padding">
-                        <p class="white-space-normal w3-large">Izraeli Boneza holds a bachelor's degree in social work and goes studies creativity through Filadelfia Bible School. Through film and photography, he will make a difference in our everyday lives. As a marketing director, he manages corporate communication with the public. He is otherwise serious about tidiness, aesthetic representation and quality in his work. </p>
+                        <p class="white-space-normal w3-large">Izraeli Boneza holds a bachelor's degree in social work and goes studies creativity through Filadelfia Bible School. Through film and photography, he will make a difference in our everyday lives. As a marketing director, he manages corporate communication with the public. He is otherwise serious about tidiness, aesthetic representation and quality in his work.</p>
                         <p><a href="https://www.linkedin.com/in/izraeli-boneza-4386a1119/" class="text-underline hover-golden-text">Izraeli’s profile on LinkedIn </a></p>
                     </div>
                 </div>
@@ -151,8 +146,7 @@
     <div class="w3-content w3-center w3-xlarge w3-container w3-padding-32">
         <h1>Join the team!</h1>
         <p>InMarket wants to help the Norwegian labor market increase in diversity.</p>
-
-            <p>Our vision is: "To influence the Norwegian education and work environment so that a common effort to increased competence, culture and diversity can lift the Norwegian market in the global context."  </p>
+        <p>Our vision is: "To influence the Norwegian education and work environment so that a common effort to increased competence, culture and diversity can lift the Norwegian market in the global context."</p>
         <a href="om.php" class="w3-right black-button w3-card">Vision and goals</a>
     </div>
 
@@ -168,12 +162,12 @@
         <div class="job-opening w3-padding-32" style="display:none;" id="webdesigner">
             <div class="w3-row w3-container w3-large">
                 <div class="w3-col m6">
-                    <h3>UX- og UI-designer</h3>
-                    <p>Deltid</p>
-                    <p>Som UX- og UI-designer hos oss vil du være med på å forme utseendet og brukeropplevelsen både på plattformen vår og nettsidene våre.</p>
-                    <p>Vi ser etter deg som klarer å sette deg selv i brukerens posisjon, er kreativ og kan samarbeide godt i et team.</p>
-                    <p>Jobben er per dags dato ikke lønnet, vi belønner heller ansatte med opsjoner til aksjeandeler i selskapet.</p>
-                    <p>1 ledig stilling</p>
+                    <h3>UX- and UI-designer</h3>
+                    <p>Part time</p>
+                    <p>As a UX and UI designer at InMarket, you will help shape the look and the user experience both on our platform and on our website.</p>
+                    <p>We are looking for those who are able to put themselves in the end user's position, are creative and can work well in a team.</p>
+                    <p>The position is unpaid as of now. We reward employees with stock options in the company.</p>
+                    <p>1 open position</p>
                 </div>
                 <div class="w3-col m6">
                     <div class="videoWrapper">
@@ -188,12 +182,12 @@
         <div class="job-opening w3-padding-32" style="display:none;" id="developer">
             <div class="w3-row w3-container w3-large">
                 <div class="w3-col m6">
-                    <h3>Utvikler</h3>
-                    <p>Deltid</p>
-                    <p>Som utvikler vil du hovedsakelig jobbe med den kommende plattformen vår. Her er det muligheter både til å jobbe på frontend, backend eller fullstack, alt etter eget ønske.</p>
-                    <p>Vi jobber med fullstack JavaScript. Hvis du har erfaring med dette er det et stort pluss, men ta gjerne kontakt selv om du ikke har det! Vi trenger deg som kan tenke systematisk, jobbe selvstendig og har høyt motivasjonsnivå.</p>
-                    <p>Jobben er per dags dato ikke lønnet, vi belønner heller ansatte med opsjoner til aksjeandeler i selskapet.</p>
-                    <p>3 ledige stillinger</p>
+                    <h3>Developer</h3>
+                    <p>Part time</p>
+                    <p>As a developer, you will mainly work with our upcoming platform. There are opportunities to work on the frontend, backend or full stack, according to your own wishes.</p>
+                    <p>We work with full stack JavaScript. It's a big plus if you have relevant experience, but feel free to contact us anyway! We need those who can think systematically, work independently and have a high level of motivation.</p>
+                    <p>The position is unpaid as of now. We reward employees with stock options in the company.</p>
+                    <p>3 open positions</p>
                 </div>
                 <div class="w3-col m6">
                     <div class="videoWrapper">
