bedadddcfc15c3624c2e3250ca4fd374a10c586d
ask <askbk@protonmail.com>
2019-03-18 10:49:24 +0100

    Gjorde ferdig footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
-<footer class="w3-container mobile-container uppercase ">
-    <div class="w3-content w3-row golden-text padding-left-100 w3-row-padding">
-        <div class="w3-col s4">
+<footer class="w3-container mobile-container uppercase">
+    <div class="w3-content w3-row golden-text padding-left-100 w3-row-padding" style="max-width:400px;">
+        <div class="w3-col s6">
             <h3 class="mobile-w3-xlarge">Oss</h3>
             <ul class="w3-ul mobile-small">
                 <li class="remove-borders remove-padding">
@@ -15,40 +15,29 @@
                 </li>
             </ul>
         </div>
-        <div class="w3-col s4">
-            <h3 class="golden-text mobile-w3-xlarge">Kontakt oss</h3>
-            <ul class="w3-ul">
-                <li class="remove-borders remove-padding">
-                    <a href="kontakt.php" class="margin-right-1em">
-                        <i class="far fa-envelope-open mobile-small"></i>
-                    </a>
+        <div class="w3-col s6">
+            <div class="w3-right">
 
-                    <a href="https://www.facebook.com/inmarketnorge" class="margin-right-1em">
-                        <i class="fab fa-facebook-f mobile-small"></i>
-                    </a>
+                <h3 class="golden-text mobile-w3-xlarge">Kontakt oss</h3>
+                <ul class="w3-ul">
+                    <li class="remove-borders remove-padding">
+                        <a href="kontakt.php" class="margin-right-1em">
+                            <i class="far fa-envelope-open mobile-small"></i>
+                        </a>
 
-                    <a href="https://www.instagram.com/inmarket_nor/" class="margin-right-1em">
-                        <i class="fab fa-instagram mobile-small"></i>
-                    </a>
-                </li>
-                <li class="mobile-small remove-padding">
-                    <i class="fas fa-map-marker-alt"></i> Drammen, Oslo
-                </li>
-            </ul>
-        </div>
-        <div class="w3-col s4">
-            <h3 class="golden-text mobile-w3-xlarge">Hjelp</h3>
-            <ul class="w3-ul mobile-small">
-                <li class="remove-borders remove-padding">
-                    <a href="#">Vanlige spørsmål og svar</a>
-                </li>
-                <li class="remove-borders remove-padding">
-                    <a href="#">Kundestøtte</a>
-                </li>
-                <li class="remove-borders remove-padding">
-                    <a href="https://www.office.com/?auth=2&home=1">Office 365</a>
-                </li>
-            </ul>
+                        <a href="https://www.facebook.com/inmarketnorge" class="margin-right-1em">
+                            <i class="fab fa-facebook-f mobile-small"></i>
+                        </a>
+
+                        <a href="https://www.instagram.com/inmarket.as/" class="margin-right-1em">
+                            <i class="fab fa-instagram mobile-small"></i>
+                        </a>
+                    </li>
+                    <li class="mobile-small remove-padding">
+                        <i class="fas fa-map-marker-alt"></i> Drammen, Oslo
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
     <p class="w3-right w3-section">InMarket AS Org.nr. 922 341 923</p>
