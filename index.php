b0cd4c8e12f02eb74c43e9e27c60b1668f0de9b2
ask <askbk@protonmail.com>
2019-03-19 14:15:02 +0100

    La til mer innhold på forsiden

--- index.php
+++ index.php
@@ -66,9 +66,95 @@
         </div>
     </div>
 
-    <div class="">
+    <div class="w3-content w3-center w3-container">
         <h3>Hvordan funker det?</h3>
-        
+
+        <div class="w3-row">
+            <div class="w3-col m6 text-align-right w3-container small-center">
+                <span class="golden-text w3-xlarge">Lag en profil</span>
+                <p>List opp dine interesser, systemet vil jobbe for å koble deg til passende arbeidsplasser. Du vil bli anbefalt til relevante bedrifter.​</p>
+            </div>
+            <div class="w3-col m2 w3-container w3-hide-small w3-hide-medium">
+                <img src="img/elever/christian-square.jpg" alt="" style="width:100%;" class="w3-circle">
+            </div>
+            <div class="w3-col m4 w3-hide-small w3-hide-medium">
+                <div class="speech-bubble-left w3-container">
+                    <p>Jeg er media elev som ser etter jobberfaring innen filmproduksjon for nett-TV.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="w3-row w3-padding-32">
+            <div class="w3-col m4 w3-hide-small w3-hide-medium">
+                <div class="speech-bubble-right w3-container">
+                    <p>
+                        Hei. Vi fra NRK ønsker å etablere kontakt med deg!
+                    </p>
+                </div>
+            </div>
+            <div class="w3-col m2 w3-container w3-hide-small w3-hide-medium">
+                <img src="img/partners/chris.jpg" alt="" style="width:100%;" class="w3-circle">
+            </div>
+            <div class="w3-col m6 text-align-left w3-container small-center">
+                <span class="golden-text w3-xlarge">Bedriftene tar kontakt</span>
+                <p>Etter at du har registrert deg er det bare å vente på tilbud fra bedrifter. I mellomtiden hjelper vi til med å forberede deg til å møte arbeidsgivere.</p>
+            </div>
+        </div>
+
+        <div class="w3-row">
+            <div class="w3-col m6 text-align-right small-center">
+                <span class="golden-text w3-xlarge">Du velger</span>
+                <p>Velg ut bedriftene som oppfyller dine ønsker og aksepter deres invitasjoner. ​</p>
+            </div>
+            <div class="w3-col m6 w3-container w3-hide-small w3-hide-medium">
+
+                <div class="w3-row w3-panel">
+                    <div class="w3-col m2">
+                        <i class="fas fa-check" style="color:#00d200;"></i>
+                    </div>
+                    <div class="w3-col m10">
+                        <div class="w3-card w3-container">
+                            Invitasjon fra NRK-Buskerud
+                        </div>
+                    </div>
+                </div>
+
+                <div class="w3-row w3-panel">
+                    <div class="w3-col m2">
+                        <i class="fas fa-check" style="color:#00d200;"></i>
+                    </div>
+                    <div class="w3-col m10">
+                        <div class="w3-card w3-container">
+                            Invitasjon fra Comfort Hotel
+                        </div>
+                    </div>
+                </div>
+
+                <div class="w3-row w3-panel">
+                    <div class="w3-col m2">
+                        <i class="fas fa-times" style="color:red;"></i>
+                    </div>
+                    <div class="w3-col m10">
+                        <div class="w3-card w3-container">
+                            Invitasjon fra InMarket Media
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="w3-row w3-padding-32">
+            <div class="w3-col m4 w3-hide-small w3-hide-medium">
+                a
+            </div>
+            <div class="w3-col m2 w3-container w3-hide-small w3-hide-medium">
+                <img src="img/elever/christian-square.jpg" alt="" style="width:100%;" class="w3-circle">
+            </div>
+            <div class="w3-col m6 text-align-left small-center">
+                <span class="golden-text w3-xlarge">Bygg opp din kompetanse</span>
+                <p>Etter bedriftsbesøk får du en validering som bekrefter dine ferdigheter. Bedriften har også mulighet til å tilby deg stilling om de ønsker.​</p>
+            </div>
+        </div>
     </div>
 
     <?php include_once 'templates/footer.php'; ?>
