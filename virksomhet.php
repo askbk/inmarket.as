b56f4e2481bf50fec538b3761573b6716a57afa1
askbk <askbk@protonmail.com>
2019-03-23 09:02:46 +0100

    La til virksomhetside

---
+++ virksomhet.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Template | Inmarket</title>
+    <?php include_once 'templates/head.php'; ?>
+</head>
+
+<body>
+    <?php
+    include_once 'templates/navigation.php';
+    include_once 'templates/cookie_notice.php';
+    ?>
+
+    <div class="w3-row margin-top-100" style="background: #181717;">
+        <div class="w3-col l7 white-text w3-large w3-container">
+            <h2>Finn din neste ansatt mens du skaper kompetanse</h2>
+            <p>På plattformen til InMarket venter mange talenter på en sjanse til å vise deg sine kvaliteter. Registrerte brukere er ivrige etter å bygge kompetanse og erfaring slik at de kan bli bedre kvalifiserte til jobb.</p>
+        </div>
+        <div class="w3-col l3 white-bg w3-center w3-container">
+            <h3>La oss møtes</h3>
+            <p>Be oss inn på møte og lær hvordan dere kan bruke InMarket</p>
+        </div>
+    </div>
+
+    <div class="w3-container w3-content w3-large w3-padding-64">
+        <div class="w3-row w3-row-padding">
+            <div class="w3-col l5">
+                <div class="w3-row">
+                    <div class="w3-col l2">
+                        <img src="img/fig/black-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/red-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/green-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/gray-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/green-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/yellow-person.png" alt="" style="width:100%;">
+                    </div>
+                </div>
+                <div class="w3-row">
+                    <div class="w3-col l2">
+                        <img src="img/fig/yellow-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/gray-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/black-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/black-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/yellow-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/gray-person.png" alt="" style="width:100%;">
+                    </div>
+                </div>
+                <div class="w3-row">
+                    <div class="w3-col l2">
+                        <img src="img/fig/green-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/black-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/yellow-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/gray-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/red-person.png" alt="" style="width:100%;">
+                    </div>
+                    <div class="w3-col l2">
+                        <img src="img/fig/green-person.png" alt="" style="width:100%;">
+                    </div>
+                </div>
+            </div>
+            <div class="w3-col l7 w3-center golden-text">
+                <p>Plattformen kobler dere til kandidater som passer deres behov. Dere betaler kun for tiden dere bruker på møter, prosjekter, intervjuer eller oppdrag med brukerne på plattformen.</p>
+                <p>Bli med å bygge kompetansen til arbeidssøkere samtidig som dere ser etter den beste kandidaten til arbeidsplassen.</p>
+            </div>
+        </div>
+        <div class="w3-row w3-row-padding">
+            <div class="w3-col l5">
+                <p>La vårt automatiserte system koble deg med potensielle arbeidstakere</p>
+            </div>
+            <div class="w3-col l7 w3-center">
+                <p>
+                    <a href="#" class="black-button w3-card">Registrer bedrift nå</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
