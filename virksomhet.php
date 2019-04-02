d9b1af823c2f1f8794ce1fcb2f96ecaf5bf3b712
ask <askbk@protonmail.com>
2019-04-02 08:37:36 +0200

    Ryddet opp

--- virksomhet.php
+++ virksomhet.php
@@ -20,61 +20,41 @@ include_once 'templates/cookie_notice.php';
                 Registrerte brukere er ivrige etter å bygge kompetanse og erfaring slik at de kan bli bedre kvalifiserte
                 til jobb.</p>
 
-
-
-
             <div class="videoWrapper " style="height:20vh;background-position:center;background-size:cover;">
                 <div style="">
                     <iframe style="position:absolute; top:-5px;" src="https://player.vimeo.com/video/327008691" width="1920" height="1080" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="vimeo-video"></iframe>
-
                 </div>
             </div>
         </article>
     </div>
     <div class="w3-col l4 white-bg w3-center w3-container" style="padding-bottom:50px;">
-        <p class="w3-xxlarge w3-margin" >La oss møtes</p>
+        <p class="w3-xxlarge w3-margin">La oss møtes</p>
         <p>Be oss inn på møte og lær hvordan dere kan bruke InMarket</p>
-        <form class="" action="index.php" method="post" id="kontaktSkjemaVirksomhet">
 
-            <!--
-            <input type="text" name="" value="" placeholder="Bedriftsnavn">
-            <input type="email" name="" value="" placeholder="E-post">
-            <input type="text" name="" value="" placeholder="Sted">-->
-
+        <form class="" method="post" id="kontaktSkjemaVirksomhet">
             <div class=" w3-content w3-center register-container">
-
                 <p>Bedrift:​</p>
                 <input type="text" name="bedriftnavn" value="" placeholder="Bedriftsnavn"
                        class="w3-input w3-animate-input" required id="inputKommune">
                 <p>E-post:</p>
                 <input type="email" name="email" value="" placeholder="deg@eksempel.no​" id="inputPlace"
                        class="w3-input w3-animate-input" required id="inputEpost">
-
-
                 <p>Kommune:</p>
                 <input type="text" name="kommune" value="" placeholder="Navnet på din kommune" required
                        class="w3-input w3-animate-input" id="inputKommune">
-
                 <label>
                     <p class="w3-margin" style="font-weight:normal">
+                        Jeg godkjenner InMarkets Vilkår for Bruk og <br>
+                        <a href="personvern.php" style="text-decoration:underline;" target="_blank">Personvernerklæring</a>.​
 
-
-                        Jeg godkjenner InMarkets Vilkår for Bruk og <br> <a
-                                href="personvern.php" style="text-decoration:underline;" target="_blank">Personvernerklæring</a>.​
-
-                        <input type="checkbox" name="personvern" id="personvern" value="student"
-                               class="klient-radio "
-                               onclick="studentSelected()" style="width:1px; position:relative; left:20px;">
+                        <input type="checkbox" name="personvern" id="personvern"
+                               style="width:1px; position:relative; left:20px;" required>
                         <i class="fas fa-user-tie w3-xlarge new-radio"></i>
                     </p>
-
                 </label>
 
+                <button class="black-button w3-card  w3-padding-8" type="submit">Avtal møte</button>
 
-
-                <button class="black-button w3-card  w3-padding-8" type="submit"
-                        ">Avtal møte
-                </button>
                 <span>
                     <strong>
                          Eller
@@ -83,11 +63,9 @@ include_once 'templates/cookie_notice.php';
                 <button class="black-button w3-card w3-padding-8 " type="submit"
                    formaction="/forhandsregistreringbedrift.php">Registrer bedriften din nå
                 </button>
-
-
-
             </div>
         </form>
+
     </div>
 </div>
 
@@ -115,9 +93,6 @@ include_once 'templates/cookie_notice.php';
     </div>
 </div>
 
-
-
-
 <div class="narrow-content w3-center w3-container">
     <h2 class="w3-margin " style="margin-top:60px!important;">Hvordan funker det?</h2>
 
@@ -127,7 +102,6 @@ include_once 'templates/cookie_notice.php';
             <p>List opp kvalifikasjonene dere trenger slik at systemet kan finne de rette kandidatene for din
                 bedrift.​</p>
         </div>
-
         <div class="w3-col m1 w3-container w3-hide-small w3-hide-medium" style="position:relative">
             <strong class="golden-text w3-xlarge">1.0</strong>
             <div class="arrow "></div><!--<div class="arrow-down"></div>-->
@@ -158,7 +132,7 @@ include_once 'templates/cookie_notice.php';
         </div>
         <div class="w3-col m1 w3-container w3-hide-small w3-hide-medium" style="position:relative">
             <strong class="golden-text w3-xlarge">2.0</strong>
-            <div class="arrow "></div><!--<div class="arrow-down"></div>-->
+            <div class="arrow"></div>
         </div>
 
         <div class="w3-col l55 text-align-left w3-container medium-center">
@@ -186,106 +160,14 @@ include_once 'templates/cookie_notice.php';
         </div>
     </div>
 
-
-
-
     <div class="w3-center w3-padding-32 w3-xlarge">
         <a href="forhandsregistreringbedrift.php" class="black-button w3-card w3-padding-16 w3-margin">Registrer
             bedriften din</a>
 
         <p class="w3-xlarge" style="padding-bottom:32px;">Kontakten brukeren har hatt med din bedrift og tilbakemelding på ferdigheter loggføres på profilen til
             brukeren. Inviter folk inn, slik at de kan bygge erfaringer som vil hjelpe dem videre i arbeidslivet.</p>
-
-
     </div>
-
-
 </div>
-
-
-<!--<div class="w3-container w3-center w3-content w3-large w3-padding-32 w3-card" style="width:70vw; margin:auto;">
-    <div class="w3-row w3-hide-small w3-hide-medium">
-        <div class="w3-col l5">
-            <div class="w3-row">
-                <div class="w3-col l2">
-                    <img src="img/fig/black-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/red-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/green-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/gray-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/green-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/yellow-person.png" alt="" style="width:100%;">
-                </div>
-            </div>
-            <div class="w3-row">
-                <div class="w3-col l2">
-                    <img src="img/fig/yellow-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/gray-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/black-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/black-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/yellow-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/gray-person.png" alt="" style="width:100%;">
-                </div>
-            </div>
-            <div class="w3-row">
-                <div class="w3-col l2">
-                    <img src="img/fig/green-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/black-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/yellow-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/gray-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/red-person.png" alt="" style="width:100%;">
-                </div>
-                <div class="w3-col l2">
-                    <img src="img/fig/green-person.png" alt="" style="width:100%;">
-                </div>
-            </div>
-        </div>
-        <div class="w3-col l7 w3-center golden-text">
-            <p>Plattformen kobler dere til kandidater som passer deres behov. Dere betaler kun for tiden dere bruker på
-                møter, prosjekter, intervjuer eller oppdrag med brukerne på plattformen.</p>
-            <p>Bli med å bygge kompetansen til arbeidssøkere samtidig som dere ser etter den beste kandidaten til
-                arbeidsplassen.</p>
-        </div>
-    </div>
-    <div class="w3-row w3-row-padding">
-        <div class="w3-col l5">
-            <p>La vårt automatiserte system koble deg med potensielle arbeidstakere</p>
-        </div>
-        <div class="w3-col l7 w3-center w3-hide-medium">
-            <p>
-                <a href="forhandsregistreringbedrift.php" class="black-button w3-card">Registrer bedrift nå</a>
-            </p>
-        </div>
-    </div>-->
-
-
 
 <?php include_once 'templates/scripts.php'; ?>
 </body>
