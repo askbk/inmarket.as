d64f0c034a8836e0d6802e4e985687363db76cca
ask <askbk@protonmail.com>
2019-04-02 09:22:04 +0200

    Fikset møteavtaling for virksomheter

--- contactVirksomheter.php
+++ contactVirksomheter.php
@@ -11,20 +11,20 @@ $bedriftnavn = $place = $email = $headers = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $bedriftnavn = test_input($_POST["name"]);
+    $bedriftnavn = test_input($_POST["bedriftnavn"]);
     $place = test_input($_POST["kommune"]);
-    $email = test_input($_POST["phone"]);
+    $email = test_input($_POST["email"]);
 }
 
 $to = "kontakt@inmarket.as";
 $subject = "Henvendelse fra $bedriftnavn";
-$message = "Henvendelse fra $bedriftnavn ved $place, tlf.: <$email>.";
+$message = "$bedriftnavn i $place, e-post: <$email> ønsker å avtale et møte.";
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text; charset=UTF-8" . "\r\n";
 $headers .= 'From: Kontaktskjema <' . $email . '>' . "\r\n";
 
 if (preg_match("#[-a-zA-Z0-9éèÉÈäöæøåÄÖÆØÅ._ ]+#",$bedriftnavn) AND
-    preg_match("#[-a-zA-Z0-9.@+!=()_:]+#", $place) AND
+    preg_match("#[-a-zA-Z0-9éèÉÈäöæøåÄÖÆØÅ._ ]+#", $place) AND
     preg_match("#[-a-zA-Z0-9.@+!=()_:]+#", $email)) {
     if(mail($to, $subject, $message, $headers)){
         echo "Takk for henvendelsen!";
