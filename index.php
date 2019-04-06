84f88c339c3121c7f6848721f677a2e636e88ff9
Filip Tangen <filiptangen1998@live.com>
2019-04-06 13:53:50 +0200

    Oppdatere logoene + Trondheim i footer

--- index.php
+++ index.php
@@ -49,32 +49,43 @@ include_once 'templates/cookie_notice.php';
 <!-- Partner logos -->
 <div class="white-text w3-padding-16 w3-center w3-content" style="width:60vw; margin 0 auto;" id="partner-slideshow">
     <div class="partner-slide" itemprop="sponsor">
-        <a href="https://www.usn.no/" target="_blank"><img src="img/logos/usn.png" alt="Universitetet i Sørøst-Norge"
+        <a href="https://www.innovasjonnorge.no/" target="_blank"><img src="img/logos/innovasjon_norge.png" alt="Innovasjon Norge"
                                                            width="100%"></a>
     </div>
     <div class="partner-slide" itemprop="sponsor">
-        <a href="https://www.akademiet.no" target="_blank"><img src="img/logos/akademiet.png" alt="Akademiet"
-                                                                width="100%"></a>
-    </div>
-    <div class="partner-slide" itemprop="sponsor">
-        <a href="https://www.drammen.kommune.no/" target="_blank"><img src="img/logos/kommune.png" alt="Drammen kommune"
-                                                                       width="100%"></a>
+        <a href="http://www.charge.no" target="_blank"><img src="img/logos/charge.png" alt="Charge" width="100%"></a>
     </div>
     <div class="partner-slide" itemprop="sponsor">
         <a href="http://www.nrk.no/buskerud" target="_blank"><img src="img/logos/nrk.png" alt="NRK Buskerud"
                                                                   width="100%"></a>
     </div>
     <div class="partner-slide" itemprop="sponsor">
-        <a href="https://www.sparkntnu.no/" target="_blank"><img src="img/logos/spark.jpg" alt="NTNU Spark"
-                                                                 width="100%"></a>
+        <a href="http://bni.no/nn/index" target="_blank"><img src="img/logos/bni.png" alt="BNI"
+                                                                  width="100%"></a>
     </div>
     <div class="partner-slide" itemprop="sponsor">
         <a href="http://nfdr.no/om-oss-medlemmer/bedrifter-i-naeringsforeningen?details=21626" target="_blank"><img
                     src="img/logos/naeringsforeningen.png" alt="Næringsforeningen" width="100%"></a>
     </div>
     <div class="partner-slide" itemprop="sponsor">
+        <a href="https://www.nordicchoicehotels.no/hotell/norge/drammen/comfort-hotel-union-brygge/?gclid=Cj0KCQjw37fZBRD3ARIsAJihSr3l1t9G6sH7FO35gTdfXQaN_FdA3OmOVDBvs6Szp1H1QrhZ8uEpgccaAgleEALw_wcB"
+           target="_blank"><img src="img/logos/unionbrygge.png" alt="Comfort Hotell Union Brygge" width="100%"></a>
+    </div>
+    <div class="partner-slide" itemprop="sponsor">
+        <a href="https://www.akademiet.no" target="_blank"><img src="img/logos/akademiet.png" alt="Akademiet"
+                                                                width="100%"></a>
+    </div>
+    <div class="partner-slide" itemprop="sponsor">
+        <a href="https://www.sparkntnu.no/" target="_blank"><img src="img/logos/spark.jpg" alt="NTNU Spark"
+                                                                 width="100%"></a>
+    </div>
+    <div class="partner-slide" itemprop="sponsor">
         <a href="https://www.allinclusivetalk.com/" target="_blank"><img src="img/logos/allinclusive.png"
                                                                          alt="All Inclusive Talk" width="100%"></a>
+    </div>
+    <div class="partner-slide" itemprop="sponsor">
+        <a href="https://www.glocalview.com/" target="_blank"><img src="img/logos/globalview.png"
+                                                                         alt="Global View" width="100%"></a>
     </div>
     <div class="partner-slide" itemprop="sponsor">
         <a href="https://nfdr.no/annet-internasjonale-drammen-aktuelt/" target="_blank"><img
@@ -84,13 +95,38 @@ include_once 'templates/cookie_notice.php';
         <a href="https://etablererveiledning.no/" target="_blank"><img src="img/logos/etablererveiledning.png" alt=""
                                                                        width="100%"></a>
     </div>
+
     <div class="partner-slide" itemprop="sponsor">
-        <a href="https://www.nordicchoicehotels.no/hotell/norge/drammen/comfort-hotel-union-brygge/?gclid=Cj0KCQjw37fZBRD3ARIsAJihSr3l1t9G6sH7FO35gTdfXQaN_FdA3OmOVDBvs6Szp1H1QrhZ8uEpgccaAgleEALw_wcB"
-           target="_blank"><img src="img/logos/unionbrygge.png" alt="Comfort Hotell Union Brygge" width="100%"></a>
+        <a href="https://cloudberrymultimedia.com/home" target="_blank"><img src="img/logos/cloudberry.png" alt="Cloud berry multimedia production"
+                                                           width="100%"></a>
     </div>
+
     <div class="partner-slide" itemprop="sponsor">
-        <a href="http://www.charge.no" target="_blank"><img src="img/logos/charge.png" alt="Charge" width="100%"></a>
+        <a href="http://www.lier.kommune.no/politikk-og-samfunnsutvikling/lierlaben/" target="_blank"><img src="img/logos/lierlaben.png" alt="Lierlaben"
+                                                                             width="100%"></a>
     </div>
+
+
+
+
+
+    <!--
+        <div class="partner-slide" itemprop="sponsor">
+            <a href="https://www.usn.no/" target="_blank"><img src="img/logos/usn.png" alt="Universitetet i Sørøst-Norge"
+                                                               width="100%"></a>
+        </div>
+
+        <div class="partner-slide" itemprop="sponsor">
+            <a href="https://www.drammen.kommune.no/" target="_blank"><img src="img/logos/kommune.png" alt="Drammen kommune"
+                                                                           width="100%"></a>
+        </div>-->
+
+
+
+
+
+
+
 </div>
 
 <div class="narrow-content w3-center w3-container" 2>
