b117b6b449d046bd52f31afcb0e0265755b738d2
samuelbigirimana <nsengi@outlook.com>
2020-02-28 08:19:57 +0100

    ferdig med oss siden intill videre

--- oss.php
+++ oss.php
@@ -23,7 +23,7 @@
                 </div>
             </div>
 
-            <div class="row r2" id="omIm">
+            <div class="row r2">
                 <div class="col">
                     <h1 data-aos="zoom-in-up"  data-aos-duration="1000">Vi bygger for fremtiden</h1>
                     <p data-aos="zoom-in-up"  data-aos-duration="1000">
@@ -34,41 +34,51 @@
                         trygghet og kunnskap om fremtiden.
                     </p>
 
-                    <button class="but butb" onclick="merTest(x)" id="merTest">Les mer</button>
-
-                    <h2>Fra start</h2>
-                    <p>
-                        InMarket har siden starten, i sommer 2017 vært fokusert på kundens behov. Vi kom fort i kontakt
+                    <h2 data-aos="zoom-in-up"  data-aos-duration="1000">Fra start</h2>
+                    <p data-aos="zoom-in-up"  data-aos-duration="1000">
+                        InMarket har siden starten, i sommer 2017 vært fokusert på kundens behov. Vi kom i tidlig kontakt
                         med aktører som Drammen Kommune, Næringsforeningen i Drammen, Akademiet, 
-                        NRK-Viken (Tidligere NRK-Buskerud), Comfort Hotel Union Brygge og USN. Siden la vi 
+                        NRK-Viken (Tidligere NRK-Buskerud), Comfort Hotel Union Brygge og USN for å teste ut 
+                        produktet vårt manuelt ved bruk av e-post og telefon. <br><br>
+
+                        I 2019 startet vi arbeidet med å digitalisere og automatisere vårt konsept slik at 
+                        det kan gjøres tilgjengelig for alle elever. Vi har utviklet en MVP med støttepenger fra 
+                        blant annet Innovasjon Norge, TrønderEnergi Billaget osv. <br><br>
+                        
+                        Vi lanserte prototypen av appen i høst 2019 og jobber nå med 
+                        å forbedre brukervennligheten og teknologien i programvaren. <br><br>
                     </p>
+
+                    <a href="appen.php" data-aos="zoom-in-up"  data-aos-duration="1000">
+                        <button class="but butb">Om Appen</button>
+                    </a>
                 </div>
             </div>
 
             <div class="row r3">
                 <div class="col">
-                    <h1>Teamet</h1>
-                    <div class="block">
+                    <h1 data-aos="zoom-in-up"  data-aos-duration="1000">Teamet</h1>
+                    <div class="block" data-aos="zoom-in-up"  data-aos-duration="1000">
                         <img src="oss/css/img-mp4/sam.jpg" alt="CEO-Founder">
                         <h2>Samuel Bigirimana - Daglig leder</h2>
                     </div>
 
-                    <div class="block">
+                    <div class="block" data-aos="zoom-in-up"  data-aos-duration="1000">
                         <img src="oss/css/img-mp4/øy.jpg" alt="Co-Founder">
                         <h2>Øyvind Fjeld - Driftsleder</h2>
                     </div>
 
-                    <div class="block">
+                    <div class="block" data-aos="zoom-in-up"  data-aos-duration="1000">
                         <img src="oss/css/img-mp4/ask.jpg" alt="CTO">
                         <h2>Ask Kolltveit - IT</h2>
                     </div>
 
-                    <div class="block">
+                    <div class="block" data-aos="zoom-in-up"  data-aos-duration="1000">
                         <img src="oss/css/img-mp4/izzy.jpg" alt="PR">
                         <h2>Izraelli Boneza - PR</h2>
                     </div>
 
-                    <div class="block">
+                    <div class="block" data-aos="zoom-in-up"  data-aos-duration="1000">
                         <img src="oss/css/img-mp4/chris.jpg" alt="Sales">
                         <h2>Christian Hellumsand - Salg</h2>
                     </div>
